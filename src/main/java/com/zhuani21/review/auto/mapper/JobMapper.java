b8ff1cca26eadf10c27e2526c56bebd9fa835d69
wgn <zhuani21@163.com>
2016-03-15 19:10:24 +0800

    f

---
+++ src/main/java/com/zhuani21/review/auto/mapper/JobMapper.java
@@ -0,0 +1,30 @@
+package com.zhuani21.review.auto.mapper;
+
+import com.zhuani21.review.auto.bean.Job;
+import com.zhuani21.review.auto.bean.JobExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface JobMapper {
+    int countByExample(JobExample example);
+
+    int deleteByExample(JobExample example);
+
+    int deleteByPrimaryKey(Integer jobId);
+
+    int insert(Job record);
+
+    int insertSelective(Job record);
+
+    List<Job> selectByExample(JobExample example);
+
+    Job selectByPrimaryKey(Integer jobId);
+
+    int updateByExampleSelective(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByExample(@Param("record") Job record, @Param("example") JobExample example);
+
+    int updateByPrimaryKeySelective(Job record);
+
+    int updateByPrimaryKey(Job record);
+}
